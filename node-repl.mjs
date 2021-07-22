ac028487d431f2453e721fcd56020db1b3bc9cac
Stan Switzer <sjswitzer@gmail.com>
2021-07-21 22:36:13 -0700

    toward supporting modules

--- node-repl.mjs
+++ node-repl.mjs
@@ -6,12 +6,11 @@
 //   4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
 //
 
-import { SchemeJS } from './SchemeJS.mjs';
+import { SchemeJS, VERSION } from './SchemeJS.mjs';
 import * as fs from 'fs';
 
 let inputFd, closeFd, oldRawMode;
 try {
-  try {
   if (process.platform === 'win32') {
     inputFd = process.stdin.fd;
     if (process.stdin.setRawMode) {
@@ -19,37 +18,39 @@ try {
       process.stdin.setRawMode(true);
     }
   } else {
-    inputFd = closeFd = fs.openSync('/dev/tty', 'rs')
-  }
-  } catch(e) {
-    console.info("Can't open termnal", e);
-    SchemeJS({ unitTest: true });  // XXX silly debugging hack
+    try {
+      inputFd = closeFd = fs.openSync('/dev/tty', 'rs');
+    } catch(e) {
+      console.info("Can't open termnal", e);
+    }
   }
   if (inputFd !== undefined) {
-  console.log(`SchemeJS 1.1 REPL. Type "." to exit.`);
-  let buffer = Buffer.alloc(2000);
-  function getLine(prompt) {
-    process.stdout.write(prompt);
-    let read = fs.readSync(inputFd, buffer);
-    let line = buffer.slice(0, read).toString();
-    while (line.endsWith('\n') || line.endsWith('\r'))
-    line = line.substr(0, line.length-1);
-    return line;
-  }
-  function readFile(path) {
-    let fileContent = fs.readFileSync(path);
-    fileContent = fileContent.toString();
-    return fileContent;
-  }
-  // It isn't really a "constructor" but can be invoked with or without "new"
-  let schemeJS = new SchemeJS( { readFile });
-  // getLine("Attach debugger and hit return!");  // Uncomment to do what it says
-  schemeJS.evalString('(define (test) (load "test.scm"))');
-  schemeJS.REPL(getLine);
+    console.log(`SchemeJS ${VERSION} REPL. Type "." to exit.`);
+    let buffer = Buffer.alloc(2000);
+    // For the REPL
+    function getLine(prompt) {
+      process.stdout.write(prompt);
+      let read = fs.readSync(inputFd, buffer);
+      let line = buffer.slice(0, read).toString();
+      while (line.endsWith('\n') || line.endsWith('\r'))
+      line = line.substr(0, line.length-1);
+      return line;
+    }
+    // for "load" and "require"
+    function readFile(path) {
+      let fileContent = fs.readFileSync(path);
+      fileContent = fileContent.toString();
+      return fileContent;
+    }
+    // It isn't really a "constructor" but can be invoked with or without "new"
+    let schemeJS = new SchemeJS({ readFile });
+    // getLine("Attach debugger and hit return!");  // Uncomment to do what it says
+    schemeJS.evalString('(define (test) (load "test.scm"))');
+    schemeJS.REPL(getLine);
   }
 } finally {
-  if (closeFd !== undefined)
-    fs.closeSync(closeFd);
   if (oldRawMode !== undefined)
     process.stdin.setRawMode(oldRawMode);
+  if (closeFd !== undefined)
+    fs.closeSync(closeFd);
 }
---
+++ unit-test.mjs
@@ -0,0 +1,11 @@
+//
+// SchemeJS Unit Tests
+//
+// Copyright 2021 Stan Switzer
+//   This work is licensed under a Creative Commons Attribution-ShareAlike
+//   4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
+//
+
+import { SchemeJS } from './SchemeJS.mjs';
+
+let schemeJS = SchemeJS({ unitTest: true });
