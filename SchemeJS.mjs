0e2bd3357a9501fd5b958e4f7d554ffa87e8046f
Stan Switzer <sjswitzer@gmail.com>
2021-07-21 19:00:25 -0700

    attempt at modules

--- SchemeJS.mjs
+++ SchemeJS.mjs
@@ -6,10 +6,7 @@
 //   4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
 //
 
-// TODO: make this a JS module
-
-"use strict";
-const VERSION = "1.1";
+export const VERSION = "1.1";
 
 //
 // Creates a SchemeJS instance, independent of any others.
@@ -19,7 +16,7 @@ const VERSION = "1.1";
 // but that should only affect that specific SchemeJS instance; others should
 // be unaffected.
 //
-function SchemeJS(schemeOpts = {}) {
+export function SchemeJS(schemeOpts = {}) {
   let readFile = schemeOpts.readFile;
   let _reportError = schemeOpts.reportError = error => console.log(error); // Don't call this one
   let reportSchemeError = schemeOpts.reportSchemeError ?? _reportError; // Call these instead
@@ -3416,53 +3413,3 @@ function SchemeJS(schemeOpts = {}) {
 
   return GlobalScope;
 } // Whew!
-
-if (typeof window === 'undefined' && typeof process !== 'undefined') { // Running under node.js
-  let fs = require('fs');
-  let inputFd, closeFd, oldRawMode;
-  try {
-    try {
-      if (process.platform === 'win32') {
-        inputFd = process.stdin.fd;
-        if (process.stdin.setRawMode) {
-          oldRawMode = process.stdin.isRaw;
-          process.stdin.setRawMode(true);
-        }
-      } else {
-        inputFd = closeFd = fs.openSync('/dev/tty', 'rs')
-      }
-    } catch(e) {
-      console.info("Can't open termnal", e);
-      SchemeJS({ unitTest: true });  // XXX silly debugging hack
-    }
-    if (inputFd !== undefined) {
-      console.log(`SchemeJS 1.1 REPL. Type "." to exit.`);
-      let buffer = Buffer.alloc(2000);
-      function getLine(prompt) {
-        process.stdout.write(prompt);
-        let read = fs.readSync(inputFd, buffer);
-        let line = buffer.slice(0, read).toString();
-        while (line.endsWith('\n') || line.endsWith('\r'))
-          line = line.substr(0, line.length-1);
-        return line;
-      }
-      function readFile(path) {
-        let fileContent = fs.readFileSync(path);
-        fileContent = fileContent.toString();
-        return fileContent;
-      }
-      // It isn't really a "constructor" but can be invoked with or without "new"
-      let schemeJS = new SchemeJS( { readFile });
-      // getLine("Attach debugger and hit return!");  // Uncomment to do what it says
-      schemeJS.evalString('(define (test) (load "test.scm"))');
-      schemeJS.REPL(getLine);
-    }
-  } finally {
-    if (closeFd !== undefined)
-      fs.closeSync(closeFd);
-    if (oldRawMode !== undefined)
-      process.stdin.setRawMode(oldRawMode);
-  }
-}
-
-console.log("done");  // breakpoint spot
