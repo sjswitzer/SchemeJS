0ea68365d5bef2fdc10b41fbd39ec5f54b4dba03
Stan Switzer <sjswitzer@gmail.com>
2021-08-15 06:30:58 -0700

    clean up gfxdemo; fix a bug in "define"!

--- gfxdemo.scm
+++ gfxdemo.scm
@@ -1,7 +1,7 @@
 ;; Web graphics in SchemeJS
 
 ;; Example adapted from https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-(define (house (? title "house")) (draw2d (canvas title 300 300)
+(define (house canvas-rendering-context)
   (line-width 10)
   (stroke-rect 75 140 150 110) ;; Wall
   (fill-rect 130 190 40 60)    ;; Door
@@ -12,12 +12,12 @@
   (line-to 250 140)
   (close-path)
   (stroke)
-))
-(house)
+)
+(house (draw2d (canvas "house" 300 300)))
 
 ;; Ship Arriving Too Late to Save a Drowning Witch
-(define (satltsadw (? size 300)) (draw2d (canvas "Too late" size (+ size 20))
-  (scale (/ size 10) (/ size 10)
+(define (satltsadw canvas-rendering-context)
+  (scale (/ (canvas-width) 10) (/ (canvas-width) 10)
     (line-width .2)
     (stroke-rect 0 0 10 9) ;; bounding box
     (move-to 0 7) (line-to 10 7) (stroke) ;; sea level
@@ -25,6 +25,6 @@
     (move-to 5.5 7) (line-to 6.5 5) (line-to 7.5 7) (stroke) ;; witch
   )
   (font "80% sans-serif")
-  (fill-text "Ship Arriving Too Late to Save a Drowning Witch" 0 size)
-))
-(satltsadw)
+  (fill-text "Ship Arriving Too Late to Save a Drowning Witch" 0 (canvas-width))
+)
+(satltsadw (draw2d (canvas "Too late" 300 320)))
