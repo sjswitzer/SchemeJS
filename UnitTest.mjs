f729747a0b7f53f711825aaf6adf19d2fb1f8f40
Stan Switzer <sjswitzer@gmail.com>
2021-08-21 09:44:14 -0700

    checkpoint

--- UnitTest.mjs
+++ UnitTest.mjs
@@ -37,7 +37,7 @@ export function run(opts = {}) {
   string = globalScope.string ?? required();
   const newScope = globalScope.newScope ?? required();
   const equal = globalScope.equal ?? required();
-  const isCons = globalScope.isCons ?? required();
+  const isList = globalScope.isList ?? required();
   const isClosure = globalScope.isClosure ?? required();
   const SchemeEvalError = globalScope.SchemeEvalError ?? required();
   const SchemeCompileError = globalScope.SchemeCompileError ?? required();
@@ -497,7 +497,7 @@ export function run(opts = {}) {
     EXPECT_ERROR(` (nth 7 '[a b c d e f g]) `, RangeError);
     EXPECT_ERROR(` (nth 7 "abcdefg") `, RangeError);
 
-    EXPECT(` (apropos "c") `, isCons);  // weak test but gets some coverage
+    EXPECT(` (apropos "c") `, isList);  // weak test but gets some coverage
     EXPECT(` (map) `, NIL);
     EXPECT(` (map (lambda (x) (* 2 x)) '(1 2 3)) `, ` '(2 4 6) `);
     EXPECT(` (map (lambda (x) (* 2 x)) '[1 2] '(3)) `, ` '(2 4 6) `);
