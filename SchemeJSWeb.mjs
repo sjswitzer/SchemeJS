f767b7d8b99ae6e02c7535667f5a8ecd3112df81
Stan Switzer <sjswitzer@gmail.com>
2021-08-29 19:03:45 -0700

    clean-up

--- SchemeJSWeb.mjs
+++ SchemeJSWeb.mjs
@@ -137,130 +137,61 @@ export function createInstance(schemeOpts = {}) {
   gfxFunction("rotate", "rotate", '[angle 0]',
      { blurb: `Rotates the coordinate system by "angle"`});
 
-  gfxFunction("begin-path", "beginPath", '',
-    {});
-
-  gfxFunction("close-path", "closePath", '',
-    {});
-
-   gfxFunction("clip", "clip", '',
-    {});
-
-  gfxFunction("is-point-in-path", "isPointInPath", '...params',
-    {});
-
-  gfxFunction("is-point-in-stroke", "isPointInStroke", '...params',
-    {});
-  
-  gfxFunction("move-to", "moveTo", '[x 0] [y 0]',
-    {});
-  
-  gfxFunction("line-to", "lineTo", '[x 1] [y 1]',
-    {});
-  
-  gfxFunction("bezier-curve-to", "bezierCurveTo", '[cpx1 1] [cpy1 0] [cpx2 0] [cpy2 1] [x 1] [y 1]',
-     {});
-
-  gfxFunction("quadratic-curve-to", "quadraticCurveTo", '[cpx 1] [cpy 0] [x 1] [y 1]',
-     {});
-     
-  gfxFunction("arc", "arc", '[x .5] [y .5] [radius .5] [start-angle 0] [end-angle (* 2 *pi*)]',
-      {});
-
-  gfxFunction("arc-to", "arcTo", '[x1 1] [y1 0] [x2 1] [y2 1] [radius 1]',
-      {});
-
+  gfxFunction("begin-path", "beginPath", '');
+  gfxFunction("close-path", "closePath", '');
+  gfxFunction("clip", "clip", '');
+  gfxFunction("is-point-in-path", "isPointInPath", '...params');
+  gfxFunction("is-point-in-stroke", "isPointInStroke", '...params');
+  gfxFunction("move-to", "moveTo", '[x 0] [y 0]');
+  gfxFunction("line-to", "lineTo", '[x 1] [y 1]');
+  gfxFunction("bezier-curve-to", "bezierCurveTo", '[cpx1 1] [cpy1 0] [cpx2 0] [cpy2 1] [x 1] [y 1]');
+  gfxFunction("quadratic-curve-to", "quadraticCurveTo", '[cpx 1] [cpy 0] [x 1] [y 1]');
+  gfxFunction("arc", "arc", '[x .5] [y .5] [radius .5] [start-angle 0] [end-angle (* 2 *pi*)]');
+  gfxFunction("arc-to", "arcTo", '[x1 1] [y1 0] [x2 1] [y2 1] [radius 1]');
   // defaults to a circle inscribing (0,0,1,1)
-  gfxFunction("ellipse", "ellipse", '[x .5] [y .5] [radius-x .5] [radius-y .5] [start-angle 0] [end-angle (* 2 *pi*)] [counterclockwise false]',
-      {});
-  
-  gfxFunction("rect", "rect", '[x 0] [y 0] [width 1] [height 1]',
-      {});
-    
+  gfxFunction("ellipse", "ellipse", '[x .5] [y .5] [radius-x .5] [radius-y .5] [start-angle 0] [end-angle (* 2 *pi*)] [counterclockwise false]');
+  gfxFunction("rect", "rect", '[x 0] [y 0] [width 1] [height 1]');
   // MDN doesn't show it but the spec says it exists!
-  gfxFunction("round-rect", "roundRect", '[x 0] [y 0] [width 1] [height 1] [radii .1]',
-      {});
-      
-  gfxFunction("fill-rect", "fillRect", '[x 0] [y 0] [width 1] [height 1]',
-      {});
-      
-  gfxFunction("clear-rect", "clearRect", '[x 0] [y 0] [width 1] [height 1]',
-      {});
-        
-  gfxFunction("stroke-rect", "strokeRect", '[x 0] [y 0] [width 1] [height 1]',
-      {});
-          
-  gfxFunction("fill-text", "fillText", '[text ""] [x 0] [y 0] [max-width]',
-      {});
-            
-  gfxFunction("measure-text", "measureText", '[text]',
-      {});
-              
-    
-  gfxProp("canvas-element", "canvas", {});
-  gfxProp("canvas-width", "canvas.width", {});
-  gfxProp("canvas-height", "canvas.height", {});
-  gfxProp("line-width", "lineWidth", {});
-  gfxProp("line-cap", "lineCcap", {});
-  gfxProp("line-join", "lineJoin", {});
-  gfxProp("miter-limit","miterLimit", {});
-
-  gfxFunction("get-line-dash", "getLineDash", '',
-      {});
-
-  gfxFunction("set-line-dash", "setLineDash", '[value []]',
-      {});
-  
-
-  gfxProp("line-dash-offset", "lineDashOffset", {});
-  gfxProp("font", "font", {});
-  gfxProp("text-align", "textAlign", {});
-  gfxProp("text-baseline", "textBaseline", {});
-  gfxProp("direction", "direction", {});
-  gfxProp("fill-style", "fillStyle", {});
-  gfxProp("stroke-style", "strokeStyle", {});
-
-  gfxFunction("create-conic-gradient", "createConicGradient", '[start-angle 0] [x 0] [y 1]',
-      {});
-
-  gfxFunction("create-linear-gradient", "createLinearGradient", '[x0 0] [y0 0] [x1 1] [y1 1]',
-      {});
-  
-  gfxFunction("create-radial-gradient", "createRadialGradient", '[x0 0] [y0 0] [r0 1] [x1 1] [y1 1] [r1 0]',
-      {});
-    
-  gfxFunction("create-pattern", "createPattern", '[image] [repetition "repeat"]',
-      {});
-      
-  gfxProp("shadow-color", "shadowColor", {});
-  gfxProp("shadow-offset-x", "shadowOffsetX", {});
-  gfxProp("shadow-offset-y", "shadowOffsetY", {});
-  gfxFunction("fill", "fill", '',
-      {});
-
-  gfxFunction("stroke", "stroke", '',
-     {});
-
-  gfxFunction("draw-focus-if-needed", "drawFocusIfNeeded", '...params',
-      {});
-
-  gfxFunction("scroll-path-into-view", "scrollPathIntoView", '...params',
-      {});
-  
-  gfxProp("global-alpha", "globalAlpha", {});
-  gfxProp("global-composite-operation", "globalCompositeOperation", {});
-
-  gfxFunction("draw-image", "drawImage", '...params',
-      {});
-  
-  gfxFunction("create-image-data", "createImageData", '...params',
-      {});
-    
-  gfxFunction("get-image-data", "getImageData", '[sx 0] [sy 0] [sw 1] [sh 1]',
-      {});
-      
-  gfxProp("image-smoothing-enabled", "imageSmoothingEnabled", {});
-  gfxProp("image-smoothing-quality", "imageSmoothingQuality", {});
+  gfxFunction("round-rect", "roundRect", '[x 0] [y 0] [width 1] [height 1] [radii .1]');
+  gfxFunction("fill-rect", "fillRect", '[x 0] [y 0] [width 1] [height 1]');
+  gfxFunction("clear-rect", "clearRect", '[x 0] [y 0] [width 1] [height 1]');
+  gfxFunction("stroke-rect", "strokeRect", '[x 0] [y 0] [width 1] [height 1]');
+  gfxFunction("fill-text", "fillText", '[text ""] [x 0] [y 0] [max-width]');
+  gfxFunction("measure-text", "measureText", '[text]');
+  gfxProp("canvas-element", "canvas");
+  gfxProp("canvas-width", "canvas.width");
+  gfxProp("canvas-height", "canvas.height");
+  gfxProp("line-width", "lineWidth");
+  gfxProp("line-cap", "lineCcap");
+  gfxProp("line-join", "lineJoin");
+  gfxProp("miter-limit","miterLimit");
+  gfxFunction("get-line-dash", "getLineDash", '');
+  gfxFunction("set-line-dash", "setLineDash", '[value []]');
+  gfxProp("line-dash-offset", "lineDashOffset");
+  gfxProp("font", "font");
+  gfxProp("text-align", "textAlign");
+  gfxProp("text-baseline", "textBaseline");
+  gfxProp("direction", "direction");
+  gfxProp("fill-style", "fillStyle");
+  gfxProp("stroke-style", "strokeStyle");
+  gfxFunction("create-conic-gradient", "createConicGradient", '[start-angle 0] [x 0] [y 1]');
+  gfxFunction("create-linear-gradient", "createLinearGradient", '[x0 0] [y0 0] [x1 1] [y1 1]');
+  gfxFunction("create-radial-gradient", "createRadialGradient", '[x0 0] [y0 0] [r0 1] [x1 1] [y1 1] [r1 0]');
+  gfxFunction("create-pattern", "createPattern", '[image] [repetition "repeat"]');
+  gfxProp("shadow-color", "shadowColor");
+  gfxProp("shadow-offset-x", "shadowOffsetX");
+  gfxProp("shadow-offset-y", "shadowOffsetY");
+  gfxFunction("fill", "fill", '');
+  gfxFunction("stroke", "stroke", '');
+  gfxFunction("draw-focus-if-needed", "drawFocusIfNeeded", '...params');
+  gfxFunction("scroll-path-into-view", "scrollPathIntoView", '...params');
+  gfxProp("global-alpha", "globalAlpha");
+  gfxProp("global-composite-operation", "globalCompositeOperation");
+  gfxFunction("draw-image", "drawImage", '...params');
+  gfxFunction("create-image-data", "createImageData", '...params');
+  gfxFunction("get-image-data", "getImageData", '[sx 0] [sy 0] [sw 1] [sh 1]');
+  gfxProp("image-smoothing-enabled", "imageSmoothingEnabled");
+  gfxProp("image-smoothing-quality", "imageSmoothingQuality");
 
   return globalScope;
 }
